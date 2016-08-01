450d3cf1133942e8873ba5c354de1fbf1e65e50a
Fritz <fre.healy@gmail.com>
2016-08-01 16:44:38 -0400

    adding cross platform url's to mako.php

--- mako.php
+++ mako.php
@@ -5,15 +5,15 @@
  * code inspired by askimet
  */
 
-// Make sure we don't expose any info if called directly
+// FROM ASKIMET : Make sure we don't expose any info if called directly
 if ( !function_exists( 'add_action' ) ) {
 	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
 	exit;
 }
 
-define( 'MAKO_PLUGIN_DIR', dirname( __FILE__ ).'\\');
+define( 'MAKO_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
 
-require_once(MAKO_PLUGIN_DIR."\class.mako.php");
+require_once(MAKO_PLUGIN_DIR."class.mako.php");
 
 //on init run mako
 add_action( 'init', array( 'Mako', 'init' ) );
