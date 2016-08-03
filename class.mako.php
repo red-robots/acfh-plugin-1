18965a5ee101eb0951832e4099163be6e56b328e
Fritz <fre.healy@gmail.com>
2016-08-03 16:09:10 -0400

    added makoautop functionality

--- class.mako.php
+++ class.mako.php
@@ -14,6 +14,9 @@ class Mako {
 		self::$initiated = true;
 		//add all mako tags for frontend processing
 		add_action("the_posts",array("Mako","tag_all_post_types"),10,2);
+		//add functionality for wpautop since it won't autop tagged posts
+		add_action("the_content",array("Mako","makoautop"),10);
+		add_action("the_excerpt",array("Mako","makoautop"),10);
 		//enqueue mako front end js
 		wp_enqueue_script("makojs",plugin_dir_url( __FILE__ )."js/mako.js",array('wp-api'),null,true);
 		//enque mako frontend stylesheet
@@ -27,5 +30,14 @@ class Mako {
 			$post->post_title = '<div class="mako" data-mako="'.$route.';title">'.$post->post_title.'</div>';
 		endforeach;
 		return $posts;
+	}
+	
+	//wrapper for auto p that allows the mako block elements contents to be autopd
+	public static function makoautop($content){
+		$matches = array();
+		if(preg_match("/^(<[^>]*?mako[^>]*?>)(.*?)(<\s*?\/\s*?div\s*?>)$/i",$content,$matches)===1){
+			$content = $matches[1].wpautop($matches[2]).$matches[3];
+		}	
+		return $content;
 	}
 }
